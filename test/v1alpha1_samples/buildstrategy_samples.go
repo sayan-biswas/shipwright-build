e3d7158e72c632048254bb7d47bd5b9a0acf09c3
sayan-biswas <2537717+sayan-biswas@users.noreply.github.com>
2024-08-07 00:40:59 +0000

    Bump gcr.io/go-containerregistry/crane from 0.19.1 to 0.20.2

--- test/v1alpha1_samples/buildstrategy_samples.go
+++ test/v1alpha1_samples/buildstrategy_samples.go
@@ -379,7 +379,7 @@ metadata:
 spec:
   buildSteps:
   - name: store-tarball
-    image: gcr.io/go-containerregistry/crane:v0.19.1
+    image: gcr.io/go-containerregistry/crane:v0.20.2
     command:
     - crane
     args:
